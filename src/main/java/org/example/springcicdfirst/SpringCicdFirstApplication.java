026d29e1a929eaa1d19f10c4b50f191cddfdcb28
gdrffg <gdrffg@gmail.com>
2025-03-06 21:06:28 +0900

    init

---
+++ src/main/java/org/example/springcicdfirst/SpringCicdFirstApplication.java
@@ -0,0 +1,13 @@
+package org.example.springcicdfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCicdFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springcicdfirst/SpringCicdFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springcicdfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
